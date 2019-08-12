11a3f981482422db86733456cfdb0b071e6bca63
Sumaer Jolly <sumaerjolly@gmail.com>
2019-08-12 17:45:27 +0530

    Sessions Controller and Sign in Added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user 
+      current_user = @user 
+    end
   end 
 
   def destroy
+    log_out 
   end 
 end
